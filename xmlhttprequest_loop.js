91713388180a7f5feac683269d0d9b8b2a3ad410
SriKeerthiGnanaPrakash <srikeerthiraj@yahoo.com>
2021-11-27 11:45:27 +0530

    Add files via upload

--- xmlhttprequest_loop.js
+++ xmlhttprequest_loop.js
@@ -1,14 +1,22 @@
 var request=new XMLHttpRequest();
+var content='';
 request.open('Get','https://jsonplaceholder.typicode.com/posts');
 request.send();
 request.onload=function(){
 
 var data = JSON.parse(this.response);
-
+html_start ="<table>  <tr><th>id</th><th>userId</th><th>title</th><th> body</th></tr>";
    for(let i=0;i<data.length;i++){
-      console.log('id',data[i].id);
-      console.log('userId',data[i].userId);
-      console.log('title',data[i].title);
-      console.log('body',data[i].body);
+    //   console.log('id',data[i].id);
+    //   console.log('userId',data[i].userId);
+    //   console.log('title',data[i].title);
+    //   console.log('body',data[i].body);
+    content += "<tr><td>"+data[i].id +"</td> <td>"+data[i].userId+"</td> <td>"+ data[i].title+"</td> <td>"+data[i].body+"</td></tr>";
   } 
+  html_end = "</table>";
+  html = html_start + content +html_end; 
+//   alert(html)
+  document.getElementById('postdata').innerHTML = html;
+
+
 }
